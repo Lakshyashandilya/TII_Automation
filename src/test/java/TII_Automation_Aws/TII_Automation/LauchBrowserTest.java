5e752562bc3c79017496274eaafb137142ebc66c
Lakshyashandilya <88477484+Lakshyashandilya@users.noreply.github.com>
2023-05-01 11:44:18 +0530

    New Code Update

--- src/test/java/TII_Automation_Aws/TII_Automation/LauchBrowserTest.java
+++ src/test/java/TII_Automation_Aws/TII_Automation/LauchBrowserTest.java
@@ -9,18 +9,41 @@ import org.testng.annotations.Test;
 
 public class LauchBrowserTest {
 
+// 	@Test
+// 	public void OpenBrowser()  {
+// 		WebDriver driver;
+// 		System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
+// 		//System.setProperty("webdriver.gecko.driver", ".\\Driver\\geckodriver.exe");
+
+// 		//Set Firefox Headless mode as TRUE
+// 		FirefoxOptions options = new FirefoxOptions();
+// 		options.setHeadless(true);
+
+// 		//pass the options parameter in the Firefox driver declaration
+// 		driver = new FirefoxDriver(options);
+// 		driver.get("https://www.google.com");
+// 		System.out.println("Title of the page is: " + driver.getTitle());
+// 		System.out.println("Title of the page is: " + driver.getTitle());
+// 		System.out.println("New Updates to be printed on console");
+
+// 		System.out.println("TestCase 1");
+
+// 		driver.close();
+
+// 	}
 	@Test
 	public void OpenBrowser()  {
 		WebDriver driver;
 		System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
 		//System.setProperty("webdriver.gecko.driver", ".\\Driver\\geckodriver.exe");
 
-		//Set Firefox Headless mode as TRUE
-		FirefoxOptions options = new FirefoxOptions();
-		options.setHeadless(true);
+// 		//Set Firefox Headless mode as TRUE
+// 		FirefoxOptions options = new FirefoxOptions();
+// 		options.setHeadless(true);
 
 		//pass the options parameter in the Firefox driver declaration
-		driver = new FirefoxDriver(options);
+// 		driver = new FirefoxDriver(options);
+		driver = new FirefoxDriver();
 		driver.get("https://www.google.com");
 		System.out.println("Title of the page is: " + driver.getTitle());
 		System.out.println("Title of the page is: " + driver.getTitle());
